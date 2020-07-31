9a61db755c9585b5bd8ba9c797bdec2e29a4bb69
Jacob Atam <jacob.atam@gmail.com>
2020-07-31 19:50:48 +0100

    Added system setting

---
+++ database/factories/SettingsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\SystemSetting;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(SystemSetting::class, function (Faker $faker) {
+    return [
+        'site_name' => 'NollyFlix',
+        'store_logo' => 'https://nollyflix.tv/images/logo/NF00.png',
+        'allow_multi_currency' => true,
+        'alert_email' => 'jacob.atam@gmail.com',
+    ];
+});
---
+++ database/seeds/SystemSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SystemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\SystenSetting::class, 1)->create();
+    }
+}
