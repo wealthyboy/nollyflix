e11e30e648429ae8eba43d160e03428abc26f15a
Jacob Atam <jacob.atam@gmail.com>
2020-08-14 19:25:19 +0100

    Added cart

--- app/OrderedMovie.php
+++ app/OrderedMovie.php
@@ -5,8 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderedMovie extends Model
-{
-      /**
+{   
+
+    protected $fillable = ['order_id','quantity','total','cancelled','status','video_id','price','purchase_type'];
+
+    /**
      * The videos bought that belong to the video.
     */
     public function video()
