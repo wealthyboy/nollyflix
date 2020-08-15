1b603e534fa1a03dff910c87fffc97f245a7bc4c
Jacob Atam <jacob.atam@gmail.com>
2020-08-15 05:11:26 +0100

    Added cart

--- resources/views/thankyou/index.blade.php
+++ resources/views/thankyou/index.blade.php
@@ -9,7 +9,7 @@
         <div class="signup--middle">                    
             <h1 class="">Thank you for buying from  us </h1>
             <p class="large">Your order has been received .</p>
-            <a href="{{ route('profiles.watchlists') }}" class="btn btn--gray space-t--2">Go to watch lists</a>
+            <a href="{{ route('profiles.watchlists') }}" class="btn btn--gray space-t--2">Go to your watch lists</a>
         </div>
     </div>
 </div>
