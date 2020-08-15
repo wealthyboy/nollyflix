eecd465541dac941ab59962d688d6b10073b9b4d
Jacob Atam <jacob.atam@gmail.com>
2020-08-15 04:09:27 +0100

    Added cart

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -50,7 +50,7 @@ class OrdersController extends Controller{
 
 	public function show($id) { 
        $order       =  Order::find($id);
-       $sub_total   =  $order->ordered_products[0]->sum_items($order->id)->items_total;
+       $sub_total   =  $order->ordered_movies[0]->sum_items($order->id)->items_total;
 	   $statuses    =  static::order_status();
 	   return view('admin.orders.show',compact('statuses','order','sub_total'));
 	}
