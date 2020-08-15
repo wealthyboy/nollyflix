efbb402df7c7924ef16def70bd59c8776d34d317
Jacob Atam <jacob.atam@gmail.com>
2020-08-15 04:12:13 +0100

    Added cart

--- app/OrderedMovie.php
+++ app/OrderedMovie.php
@@ -9,6 +9,9 @@ class OrderedMovie extends Model
 
     protected $fillable = ['order_id','quantity','total','cancelled','status','video_id','price','purchase_type'];
 
+    public $appends = ['order_price'];
+
+
     /**
      * The videos bought that belong to the video.
     */
@@ -16,4 +19,23 @@ class OrderedMovie extends Model
     {
         return $this->belongsTo('App\Video');
     }
+
+
+	
+
+
+	
+	public function order(){
+		return $this->belongsTo('App\Order');
+	}
+
+	public function getOrderPriceAttribute(){
+		return  $this->price;
+	}
+
+
+	public  function sum_items($order_id) {   
+		$total = \DB::table('ordered_product')->select(\DB::raw('SUM(total) as items_total'))->where('order_id',$order_id)->get();
+		return 	$total = $total[0];
+	}
 }
