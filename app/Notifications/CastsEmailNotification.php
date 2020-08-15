235bb67920eea2464e80260cc37bef91c0550892
Jacob Atam <jacob.atam@gmail.com>
2020-08-15 11:42:58 +0100

    Updated users

--- app/Notifications/CastsEmailNotification.php
+++ app/Notifications/CastsEmailNotification.php
@@ -47,7 +47,7 @@ class CastsEmailNotification extends Notification implements ShouldQueue
                         ->line('We have added you to nollyflix. ') 
                         ->line('Your username is: '.$this->casts->casts['email'])
                         ->line('Your password is: '.$this->casts->casts['password'])
-                        ->line('Your profile page is: https://nollyflix.tv/profile/'.str_slug($this->casts->casts['first_name'].' '.$this->casts->casts['last_name'])) 
+                        ->line('Your profile page is: https://nollyflix.tv/'.str_slug($this->casts->casts['username'])) 
                         ->line('Your can  login and check how your movies are doing through your dashboard') 
                         ->subject("Your profile have been created")
                         ->action('Click here to visit your dashboard', url('/profile'))
