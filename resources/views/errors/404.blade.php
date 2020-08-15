3ed34b57008f347c75c00a147fbc1e31fc688655
Jacob Atam <jacob.atam@gmail.com>
2020-08-15 12:50:42 +0100

    Updated users

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,30 +4,18 @@
 
 
 <div class="page-contaiter vh-100">
-            <!--Content-->
-            <section class="sec-padding--lg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4 offset-md-4">
-                            <div class="error-page text-center">
-                                <h1>404</h1>
-                                <h3>Oops, page not found.</h3>
-                                <p class="large">It looks like nothing was found at this location. Click the link below to return home.</p>
-                                <!--<div class="widget">
-                                    <form class="search-form">
-                                        <input class="search-field input--lg" placeholder="Search.." value="" name="s" type="search">
-                                        <button type="submit" class="search-button btn btn--primary btn--lg">
-                                            <i class="fa fa-search"></i>
-                                        </button>
-                                    </form>
-                                </div>-->
-                                <a href="/" class="btn btn--gray space-t--2">Back to Home</a>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </section>
-            <!--End Content-->
+    <!--Content-->
+    <section class="sec-padding--lg">
+        <div class="container-fluid">
+            <div class="signup--middle"> 
+                <h1>404</h1>
+                <h3>Oops, page not found.</h3>
+                <p class="large">It looks like nothing was found at this location. Click the link below to return home.</p>
+                <a href="/" class="btn btn--gray space-t--2">Back to Home</a>
+            </div>
         </div>
+
+    </section>
+    <!--End Content-->
+</div>
 @endsection
