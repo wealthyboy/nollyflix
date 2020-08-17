5411068677fbfa6439d0c59182c8b6a37b30c03a
Jacob Atam <jacob.atam@gmail.com>
2020-08-17 18:02:15 +0100

    Updated users

--- app/Http/Controllers/Checkout/CheckoutController.php
+++ app/Http/Controllers/Checkout/CheckoutController.php
@@ -35,7 +35,7 @@ class CheckoutController extends Controller
 		
 	public function  index()  { 
 		$carts =  Cart::all_items_in_cart();
-		dd($carts);
+		dd($carts[0]->orders);
 		$csrf = json_encode(['csrf' => csrf_token()]);
 		$currency =  Helper::getCurrency();
 		return view('checkout.index',[
