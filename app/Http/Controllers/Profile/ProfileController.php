a819eec6bedf57fd0f5bd4f53980511dea4aa1b4
Jacob Atam <jacob.atam@gmail.com>
2020-08-18 15:38:49 +0100

    Updated ordered

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -44,6 +44,7 @@ class ProfileController extends Controller
         if ($user){
             session(['content_owner_id' => $user->id]);
         }
+        dd(session('content_owner_id'));
         return view('profile.profile',compact('user'));
     }
 
