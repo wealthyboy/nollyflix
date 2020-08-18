6a1f678d6d4540002ea23a81805748dba1ba3f36
Jacob Atam <jacob.atam@gmail.com>
2020-08-18 13:10:28 +0100

    Updated ordered

--- resources/views/partials/profile_nav_link.blade.php
+++ resources/views/partials/profile_nav_link.blade.php
@@ -1,6 +1,6 @@
 <ul id="dashboard-sub-menu">
     <li class="{{  $current  == 'profile' ? 'current' : '' }}"><a href="{{ route('profile.index') }}">Account Settings</a></li>
-    @if (!$user->isSubscriber())
+    @if (!$user->isSubscriber()  || !$user->isAdmin())
         <li class="{{ $current == 'videos' ? 'current' : '' }}" ><a href="{{ route('profiles.videos') }}">Videos({{ $user->profile_videos->count() }})</a></li>
     @endif
     <li class="{{ $current == 'watchlists' ? 'current' : '' }}"><a href="{{ route('profiles.watchlists') }}">Watchlist({{ $user->movies->count() }})</a></li>
