3af58b0d641b732416dbf7510fba0de9cb62bccb
Jacob Atam <jacob.atam@gmail.com>
2020-08-19 22:16:59 +0100

    Updated order

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div >
+    <div class="position-absolute" id=""><<< Back</div>
     <video  id="background_video" controls>
         <source src="{{ $video->link }}" type="video/mp4">
     </video>
