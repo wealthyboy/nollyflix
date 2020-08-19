2eca9b5b61ee982ec905eb72fccc6c30956acc8d
Jacob Atam <jacob.atam@gmail.com>
2020-08-19 18:40:00 +0100

    Updated order

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -67,6 +67,15 @@
                 </div>
             </div>
             <!---->
+            <div class="background-image">
+                <div class="container-fluid">
+                    <div class="signup--middle">                    
+                        <h1 class="">Thank you for buying from  us </h1>
+                        <p class="large">Your order has been received .</p>
+                        <a href="{{ route('profiles.watchlists') }}" class="btn btn--gray space-t--2">Go to your watch lists</a>
+                    </div>
+                </div>
+            </div>
         @else
             <section class="vh-100">
 
