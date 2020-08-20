0a06cedfd1f796b20b1ab101bdf0d9e920b241f2
Jacob Atam <jacob.atam@gmail.com>
2020-08-20 04:44:03 +0100

    Updated order

--- resources/views/watch/video_expired.blade.php
+++ resources/views/watch/video_expired.blade.php
@@ -58,11 +58,10 @@
                     <div id="vayvo-video-post-content">
                         <p>{!! optional($video)->description !!}</p>
                     </div><!-- #vayvo-video-post-content -->
-                    <div id="video-post-buttons-container">
-                        <a href="{{ optional($video)->preview_link }}" class=""  data-fancybox id="video-post-play-text-btn"><i class="far fa-play-circle"></i>Play Trailer </a>
-                    </div><!-- close #video-post-buttons-container -->
+                    
 
                     <div id="video-post-buttons-container">
+                        <a href="{{ optional($video)->preview_link }}" class=""  data-fancybox id="video-post-play-text-btn"><i class="far fa-play-circle"></i>Play Trailer </a>
                         <a href="#" class="buy-video"   data-prop="{{ $video }}"  data-type="buy" id="video-post-play-text-btn"><i class="fas fa-shopping-cart"></i>Buy  {{ $video->currency }}{{ number_format($video->buy_price) }} </a>
                         <a href="#" class="rent-video"  data-prop="{{ $video }}"  data-type="rent"id="video-post-play-text-btn"><i class="fas fa-shopping-cart"></i>Rent  {{ $video->currency }}{{ number_format($video->rent_price) }}</a>
                         <div class="clearfix"></div>
