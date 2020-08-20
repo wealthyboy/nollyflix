107aa4116596fef853ce4d9126bc3b302de0238b
Jacob Atam <jacob.atam@gmail.com>
2020-08-20 03:45:22 +0100

    Updated order

--- resources/views/watch/video_expired.blade.php
+++ resources/views/watch/video_expired.blade.php
@@ -9,7 +9,7 @@
                 <div class="progression-studios-video-index-container">
                     <a href="#">
                 
-                        <div class="progression-studios-video-feaured-image"><img src="{{ $video->video->tn_poster }}" alt="{{ $video->video->title }}"></div>
+                        <div class="progression-studios-video-feaured-image"><img src="{{ $video->poster }}" alt="{{ $video->title }}"></div>
             
                         <div class="progression-video-index-content no-background">
                             <div class="progression-video-index-table">
@@ -25,16 +25,10 @@
                             </div><!-- close .progression-video-index-table -->
                         </div><!-- close .progression-video-index-content -->
                         <div class="video-index-border-hover"></div>
-                        <div class="progress mt-2">
-                            <div class="progress-bar bg-danger" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
+                        
                         <div class="mr-2">
-                            <h3 class="progression-video-title">Type: {{ $video->purchase_type == 'rent' ? 'Rented' : 'Bought'}}</h3>
-                            <h3 class="progression-video-title">Price: {{ $video->price }}</h3>
 
-                            @if ($video->purchase_type == 'rent')
-                                <h3 class="progression-video-title">Expires:  {{  $video->isVideoRentExpired() ? 'Video Expired' : $video->videoExpires()->format('d/m/y')  }}</h3>
-                            @endif
+                           
                         </div>
                         
                 
