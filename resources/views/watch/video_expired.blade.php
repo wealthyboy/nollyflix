298759abd2d000f70f5cd982ff04f9b92dd4f7d0
Jacob Atam <jacob.atam@gmail.com>
2020-08-20 04:41:38 +0100

    Updated order

--- resources/views/watch/video_expired.blade.php
+++ resources/views/watch/video_expired.blade.php
@@ -59,10 +59,6 @@
                         <p>{!! optional($video)->description !!}</p>
                     </div><!-- #vayvo-video-post-content -->
 
-
-
-
-
                     <div id="video-post-buttons-container">
                         <a href="{{ optional($video)->preview_link }}" class=""  data-fancybox id="video-post-play-text-btn"><i class="far fa-play-circle"></i>Play Trailer </a>
                         <a href="#" class="buy-video"   data-prop="{{ $video }}"  data-type="buy" id="video-post-play-text-btn"><i class="fas fa-shopping-cart"></i>Buy  {{ $video->currency }}{{ number_format($video->buy_price) }} </a>
@@ -71,6 +67,44 @@
                     </div><!-- close #video-post-buttons-container -->
                 </div>
             </div>
+
+            <div id="video-more-like-this-details-section">
+                
+                @if($video->related_videos->count())
+                <h3 id="more-videos-heading">More Like This</h3>
+            
+                <div class="progression-studios-elementor-carousel-container progression-studios-always-arrows-on">
+                    <div id="progression-video-carousel" class="owl-carousel progression-carousel-theme">
+                    @foreach( $video->related_videos as $video)
+
+                        <div class="item">
+                            <div class="progression-studios-video-index-container">
+                                <a href="/browse/{{ $video->slug }}">
+                                    <div class="progression-studios-video-feaured-image"><img src="{{ $video->tn_poster }}" alt="{{ $video->title }}"></div>
+                                    <div class="progression-video-index-content no-background">
+                                        <div class="progression-video-index-table">
+                                            <div class="progression-video-index-vertical-align">
+                                                <h2 class="progression-video-title"></h2>
+                                                <div class="clearfix"></div>                                    
+                                            </div><!-- close .progression-video-index-vertical-align -->
+                                        </div><!-- close .progression-video-index-table -->
+                                    </div><!-- close .progression-video-index-content -->
+                                    <div class="video-index-border-hover"></div>
+                                </a>
+                            </div><!-- close .progression-studios-video-index-container  -->
+                        </div><!-- close .item -->
+                        
+                        @endforeach
+
+                    </div><!-- close #progression-video-carousel -->
+                </div><!-- close .progression-studios-elementor-carousel-container  -->
+
+            @else
+            @endif 
+                
+                <div style="height:10px;"></div>
+
+            </div><!-- close #video-more-like-this-details-section -->
         </div><!-- close .row --> 
         
     </div><!-- close .container -->
