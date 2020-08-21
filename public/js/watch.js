bb790148802c7a305ada8ef09234a858efe08f89
Jacob Atam <jacob.atam@gmail.com>
2020-08-21 04:44:41 +0100

    Updated order

--- public/js/watch.js
+++ public/js/watch.js
@@ -1,46 +1,36 @@
 
 
 
-//       var video = document.querySelector('video');
 
+const NUM_CHUNKS = 5;
 
-// var assetURL = 'https://vod-progressive.akamaized.net/exp=1597986478~acl=%2A%2F542258040.mp4%2A~hmac=cdf71489a2fc427ad74981ab5cd041011158b2525ff19d1dd9f7a0335d7142b6/vimeo-prod-skyfire-std-us/01/3821/6/169107226/542258040.mp4?filename=STALKER+SCREENER.mp4';
-// // Need to be specific for Blink regarding codecs
-// // ./mp4info frag_bunny.mp4 | grep Codec
-// var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+var video = document.querySelector('video');
+video.src = video.webkitMediaSourceURL;
+console.log(video.webkitMediaSourceURL)
 
-// if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
-//   var mediaSource = new MediaSource;
-//   //console.log(mediaSource.readyState); // closed
-//   video.src = URL.createObjectURL(mediaSource);
-//   mediaSource.addEventListener('sourceopen', sourceOpen);
-// } else {
-//   console.error('Unsupported MIME type or codec: ', mimeCodec);
-// }
+video.addEventListener('webkitsourceopen', function(e) {
+  var chunkSize = Math.ceil(file.size / NUM_CHUNKS);
 
-// function sourceOpen (_) {
-//   //console.log(this.readyState); // open
-//   var mediaSource = this;
-//   var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-//   fetchAB(assetURL, function (buf) {
-//     sourceBuffer.addEventListener('updateend', function (_) {
-//       mediaSource.endOfStream();
-//       video.play();
-//       //console.log(mediaSource.readyState); // ended
-//     });
-//     sourceBuffer.appendBuffer(buf);
-//   });
-// };
+  // Slice the video into NUM_CHUNKS and append each to the media element.
+  for (var i = 0; i < NUM_CHUNKS; ++i) {
+    var startByte = chunkSize * i;
 
-// function fetchAB (url, cb) {
-//   console.log(url);
-//   var xhr = new XMLHttpRequest;
-//   xhr.open('get', url);
-//   xhr.responseType = 'arraybuffer';
-//   xhr.onload = function () {
-//     cb(xhr.response);
-//   };
-//   xhr.send();
-// };
+    // file is a video file.
+    var chunk = file.slice(startByte, startByte + chunkSize);
+
+    var reader = new FileReader();
+    reader.onload = (function(idx) {
+      return function(e) {
+        video.webkitSourceAppend(new Uint8Array(e.target.result));
+        logger.log('appending chunk:' + idx);
+        if (idx == NUM_CHUNKS - 1) {
+          video.webkitSourceEndOfStream(HTMLMediaElement.EOS_NO_ERROR);
+        }
+      };
+    })(i);
+
+    reader.readAsArrayBuffer(chunk);
+  }
+}, false);
 
 
--- resources/views/browse/show.blade.php
+++ resources/views/browse/show.blade.php
@@ -1,39 +1,74 @@
 @extends('layouts.access')
 
 @section('content')
-
-
-
-<div id="video-page-title-pro" style="background-image:url({{ optional($video)->poster }});">
-    <a class="video-page-title-play-button"  data-fancybox  href="{{ optional($video)->preview_link }}"><i class="fas fa-play"></i></a>
-    <div id="video-page-title-gradient-base"></div>
-</div><!-- close #video-page-title-pro -->
-
 <div id="content-pro">
-			
-    <div class="container custom-gutters-pro">
-        <div id="video-post-container">
-            <h1 class="video-post-heading-title">{{ optional($video)->title }}</h1>
-            <div class="clearfix"></div>
-            
-            <ul id="video-post-meta-list">
-                <li><a href="/">{{ $video->genres[0]->name }}</a></li>
-                <li id="video-post-meta-year">{{ $video->created_at->format('Y') }}</li>
-                <li id="video-post-meta-rating"><span>{{ $video->film_rating }}</span></li>
-            </ul>
-            <div class="clearfix"></div>
+    <div class="container-fluid custom-gutters-pro">
+        <div class="row">
+           
+            <div class="col col-12 col-md-7 col-lg-7">
+                <div class="progression-studios-video-index-container">
+                
+                    <div class="progression-studios-video-feaured-image"><img src="{{ $video->poster }}" alt="{{ $video->title }}"></div>
 
-            <div id="vayvo-video-post-content">
-                <p>{!! optional($video)->description !!}</p>
-            </div><!-- #vayvo-video-post-content -->
+                    <div class="progression-video-index-content no-background">
+                        <div class="progression-video-index-table">
+                            <div class="progression-video-index-vertical-align">
+                    
+                                <div class="clearfix"></div>
+                                <ul class="video-index-meta-taxonomy">
+                                    <li></li>
+                                </ul>												
+                                <div class="clearfix"></div>
+                    
+                            </div><!-- close .progression-video-index-vertical-align -->
+                        </div><!-- close .progression-video-index-table -->
+                    </div><!-- close .progression-video-index-content -->
+                    <div class="video-index-border-hover"></div>
+                    
+                    <div class="mr-2">
 
-            <div id="video-post-buttons-container">
-                <a href="#" class="buy-video"   data-prop="{{ $video }}" data-user="{{ $user }}" data-type="buy" id="video-post-play-text-btn"><i class="fas fa-shopping-cart"></i>Buy  {{ $video->currency }}{{ number_format($video->buy_price) }} </a>
-                <a href="#" class="rent-video"  data-prop="{{ $video }}" data-user="{{ $user }}" data-type="rent"id="video-post-play-text-btn"><i class="fas fa-shopping-cart"></i>Rent  {{ $video->currency }}{{ number_format($video->rent_price) }}</a>
-                <div class="clearfix"></div>
-            </div><!-- close #video-post-buttons-container -->
+                        
+                    </div>
+                        
+                
+                
+                </div><!-- close .progression-studios-video-index-container -->
+            </div><!-- close .col -->
 
-    
+            <div class="col col-12 col-md-5 col-lg-5">
+                <div class="video-title-container ml-3">
+                    <h1 class="video-post-heading-title">{{ optional($video)->title }}</h1>
+                    <div class="clearfix"></div>
+                
+                    <ul id="video-post-meta-list">
+                        <li><a href="/">{{ $video->genres[0]->name }}</a></li>
+                        <li id="video-post-meta-year">{{ $video->created_at->format('Y') }}</li>
+                        <li id="video-post-meta-rating"><span>{{ $video->film_rating }}</span></li>
+                    </ul>
+                    <div class="clearfix"></div>
+
+                    <div id="vayvo-video-post-content">
+                        <div class="content-sidebar-short-description">{{ $video->duration }}</div>
+                    </div><!-- #vayvo-video-post-content -->
+
+                    <div id="vayvo-video-post-content">
+                        <div class="content-sidebar-short-description"> Starring: {{ implode(' , ',$video->casts()->pluck('name','last_name')->toArray() )}}</div>
+                    </div><!-- #vayvo-video-post-content -->
+
+                    <div id="vayvo-video-post-content">
+                        <p>{!! optional($video)->description !!}</p>
+                    </div><!-- #vayvo-video-post-content -->
+                    
+
+                    <div id="video-post-buttons-container">
+                        <a href="{{ optional($video)->preview_link }}" class=""  data-fancybox id="video-post-play-text-btn"><i class="far fa-play-circle"></i>Play Trailer </a>
+                        <a href="#" class="buy-video"   data-prop="{{ $video }}"  data-type="buy" id="video-post-play-text-btn"><i class="fas fa-shopping-cart"></i>Buy  {{ $video->currency }}{{ number_format($video->buy_price) }} </a>
+                        <a href="#" class="rent-video"  data-prop="{{ $video }}"  data-type="rent"id="video-post-play-text-btn"><i class="fas fa-shopping-cart"></i>Rent  {{ $video->currency }}{{ number_format($video->rent_price) }}</a>
+                        <div class="clearfix"></div>
+                    </div><!-- close #video-post-buttons-container -->
+                </div>
+            </div>
+
             <div id="video-more-like-this-details-section">
                 
                 @if($video->related_videos->count())
@@ -71,36 +106,8 @@
                 <div style="height:10px;"></div>
 
             </div><!-- close #video-more-like-this-details-section -->
-
-        </div><!-- close #video-post-container -->
+        </div><!-- close .row --> 
         
-        
-        
-        <div id="video-post-sidebar">
-          
-            
-            <div class="content-sidebar-section video-sidebar-section-length">
-                <h4 class="content-sidebar-sub-header">Duration</h4>
-                <div class="content-sidebar-short-description">{{ $video->duration }}</div>
-            </div><!-- close .content-sidebar-section -->
-
-
-            <div class="content-sidebar-section video-sidebar-section-length">
-                <h4 class="content-sidebar-sub-header">Starring</h4>
-               
-            </div><!-- close .content-sidebar-section -->
-            
-         
-    
-            <div class="clearfix"></div>
-        </div>
-        <!-- close #video-post-sidebar -->
-        
-        
-        <div class="clearfix"></div>
     </div><!-- close .container -->
-    
-    
-    
 </div><!-- close #content-pro -->
 @endsection
