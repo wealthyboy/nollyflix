ea2d66fffdc37ff43d7dd949898cc557851acf52
Jacob Atam <jacob.atam@gmail.com>
2020-08-23 15:50:55 +0100

    Added cart

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,20 @@
 
 @section('content')
 <div class="row">
-    <div class="col-lg-4 col-md-6 col-sm-6">
+   <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+         <div class="card-content">
+            <p class="category">Number of Movies</p>
+            <h3 class="card-title">{{ $videos }}</h3>
+         </div>
+         <div class="card-footer text-right">
+				<div class="stats">
+					<i class="material-icons text-danger">forward</i> <a href="{{ route('videos.index') }}">View</a>
+				</div>
+			</div>
+      </div>
+   </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
       <div class="card card-stats">
          <div class="card-content">
             <p class="category">Number of Subscribers</p>
@@ -16,7 +29,7 @@
       </div>
     </div>
    
-    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="col-lg-3 col-md-6 col-sm-6">
       <div class="card card-stats">
          <div class="card-content">
             <p class="category">Number of Casts</p>
@@ -29,7 +42,7 @@
 			</div>
       </div>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="col-lg-3 col-md-6 col-sm-6">
       <div class="card card-stats">
          <div class="card-content">
          <p class="category">Number of Filmer Makers</p>
@@ -44,77 +57,6 @@
     </div>
   
 </div>
-<div class="row">
-   <div class="col-lg-6 col-md-6 col-sm-6">
-      <div class="card card-stats">
-         <div class="card-content">
-            <p class="category">Videos Rented Today</p>
-            <h3 class="card-title">{{ 0 }}</h3>
-         </div>
-         <div class="card-footer text-right">
-				<div class="stats">
-					<i class="material-icons text-danger">forward</i> <a href="#">View</a>
-				</div>
-			</div>
-      </div>
-   </div>
-   <div class="col-lg-6 col-md-6 col-sm-6">
-      <div class="card card-stats">
-         <div class="card-content">
-            <p class="category">Videos Sold Today</p>
-            <h3 class="card-title">{{ 0 }}</h3>
-         </div>
-         <div class="card-footer text-right">
-				<div class="stats">
-					<i class="material-icons text-danger">forward</i> <a href="/">View</a>
-				</div>
-			</div>
-      </div>
-   </div>
-</div>
-<div class="row">
-   <div class="col-lg-3 col-md-6 col-sm-6">
-      <div class="card card-stats">
-         <div class="card-content">
-            <p class="category">Number of Movies</p>
-            <h3 class="card-title">{{ $videos }}</h3>
-         </div>
-         <div class="card-footer text-right">
-				<div class="stats">
-					<i class="material-icons text-danger">forward</i> <a href="{{ route('videos.index') }}">View</a>
-				</div>
-			</div>
-      </div>
-   </div>
 
-    <div class="col-lg-3 col-md-6 col-sm-6">
-      <div class="card card-stats">
-         <div class="card-content">
-            <p class="category">All Sales</p>
-            <h3 class="card-title">{{ 0 }}</h3>
-         </div>
-         <div class="card-footer text-right">
-				<div class="stats">
-					<i class="material-icons text-danger">forward</i> <a href="#">View</a>
-				</div>
-			</div>
-      </div>
-   </div>
-   <div class="col-lg-3 col-md-6 col-sm-6">
-      <div class="card card-stats">
-         <div class="card-content">
-            <p class="category">Sales Total</p>
-            <h3 class="card-title">{{ 0 }}</h3>
-         </div>
-         <div class="card-footer text-right">
-				<div class="stats">
-					<i class="material-icons text-danger">forward</i> <a href="#">View</a>
-				</div>
-			</div>
-      </div>
-   </div>
-  
- 
-</div>
 
 @endsection
