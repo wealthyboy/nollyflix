a412e13494f11ae89dc8d79a14eaaf7fdc60cc4f
Jacob Atam <jacob.atam@gmail.com>
2020-08-23 16:19:10 +0100

    Added cart

--- app/Category.php
+++ app/Category.php
@@ -25,7 +25,7 @@ class Category extends Model
 
     public function videos()
     {
-        return $this->belongsToMany('App\Video')->where('allow',1)->orderBy('id','DESC');
+        return $this->belongsToMany('App\Video')->where('allow',1)->latest();
     }
 
     public function getRouteKeyName(){
