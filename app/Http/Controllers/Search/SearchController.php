bd8944d3638e54b91c760e9efeb79734afec7c0e
Jacob Atam <jacob.atam@gmail.com>
2020-08-23 17:17:16 +0100

    Added cart

--- app/Http/Controllers/Search/SearchController.php
+++ app/Http/Controllers/Search/SearchController.php
@@ -39,11 +39,26 @@ class SearchController extends Controller
 						})->
 						join('categories', function($join) { 
 							$join->on('category_video.category_id','=','categories.id');
+                        })->
+                        join('genre_video', function($join) { 
+							$join->on('genre_video.video_id','=','videos.id');
+						})->
+						join('genres', function($join) { 
+							$join->on('genre_video.genre_id','=','genres.id');
+                        })
+                        ->
+                        join('cast_video', function($join) { 
+							$join->on('cast_video.video_id','=','videos.id');
+						})->
+						join('users', function($join) { 
+							$join->on('cast_video.user_id','=','users.id');
 						})
 						->where(function ($query) use ($filtered_array) {
-							$query->where('categories.name','like','%' .$filtered_array['q'] . '%')
-								->orWhere('videos.title', 'like', '%' .$filtered_array['q'] . '%');
-
+                            $query->where('categories.name','like','%' .$filtered_array['q'] . '%')
+                                ->orWhere('videos.title', 'like', '%' .$filtered_array['q'] . '%')
+                                ->orWhere('users.name', 'like', '%' .$filtered_array['q'] . '%')
+                                ->orWhere('users.last_name', 'like', '%' .$filtered_array['q'] . '%')
+                                ->orWhere('genres.name', 'like', '%' .$filtered_array['q'] . '%')
                         });
         }
 			
