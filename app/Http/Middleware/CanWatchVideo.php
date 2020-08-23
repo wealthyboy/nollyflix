d33856d5984b275671cc34c3b7366851a364017f
Jacob Atam <jacob.atam@gmail.com>
2020-08-23 11:55:08 +0100

    Added cart

--- app/Http/Middleware/CanWatchVideo.php
+++ app/Http/Middleware/CanWatchVideo.php
@@ -52,7 +52,7 @@ class CanWatchVideo
             $view->save();
         }
 
-        dd($video->videoExpires()->format('d/m/y'));
+        dd(now() > $video->videoExpires()->format('d/m/y'));
 
         if ( $video->isVideoRentExpired() ){
             return redirect()->route('watch.expired',['id' => $request->id]);
