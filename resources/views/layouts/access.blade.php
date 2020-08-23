5a9f215d7e9bef3046e2922fd8c8635320dce796
Jacob Atam <jacob.atam@gmail.com>
2020-08-23 15:31:15 +0100

    Added cart

--- resources/views/layouts/access.blade.php
+++ resources/views/layouts/access.blade.php
@@ -1,30 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 	<head>
-		<meta charset="utf-8">
-		<title>{{ isset( $page_title) ?  $page_title .' |  '.config('app.name') : 'NollyFlix' }}</title>
-
-		<link rel="icon" href="https://nollyflix.tv/favicons/cropped-nflix-32x32.png" sizes="32x32" />
-		<link rel="icon" href="https://nollyflix.tv/favicons/cropped-nflix-192x192.png" sizes="192x192" />
-		<link rel="apple-touch-icon-precomposed" href="https://nollyflix.tv/favicons/cropped-nflix-180x180.png" />
-		<meta name="msapplication-TileImage" content="https://nollyflix.tv/favicons/cropped-nflix-270x270.png" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="canonical" href="https://nollyflix.tv/">
-		<meta property="og:site_name" content="NollyFilx">
-		<meta property="og:url" content="https://nollyflix.tv">
-		<meta property="og:title" content=" NollyFlix tv">
-		<meta property="og:type" content="website">
-		<meta property="og:description" content="Watch nollywood movies online">
-		<script src='https://kit.fontawesome.com/a076d05399.js'></script> 
-		<link rel="dns-prefetch" href="//fonts.gstatic.com">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;500;700&family=Lato:wght@300;400;700&display=swap">
-		<link rel="stylesheet" href="/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/css/style.css">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
-		<link rel="stylesheet" href="/css/overide.css?version={{ str_random(6) }}">
-		<!-- CSRF Token -->
-		<meta name="csrf-token" content="{{ csrf_token() }}">
-	
+		@include('partials.styles')
 	</head>
 
 	<script>
