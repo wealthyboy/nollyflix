3cb17d59f36d68d727f7ea35de2ad2c3fc00e3d3
Jacob Atam <jacob.atam@gmail.com>
2020-08-28 21:37:49 +0100

    Added track file

--- public/js/watch.js
+++ public/js/watch.js
@@ -7,6 +7,25 @@ videojs('background_video', {
       }
 })
 
+$(function(){
+  var $refreshButton = $('#refresh');
+  var $results = $('#css_result');
+  
+  function refresh(){
+    var css = $('style.cp-pen-styles').text();
+    $results.html(css);
+  }
+
+  refresh();
+  $refreshButton.click(refresh);
+  
+  // Select all the contents when clicked
+  $results.click(function(){
+    $(this).select();
+  });
+});
+
+
 //document.addEventListener('contextmenu', event => event.preventDefault());
 
 // var video = document.querySelector('video');
