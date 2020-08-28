dcc721a57322eccfc000fd3223c43576acbb560f
Jacob Atam <jacob.atam@gmail.com>
2020-08-28 21:16:39 +0100

    Added track file

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div>
     <video id="background_video" class="video-js vjs-default-skin"  data-setup ='{}'
-        controls preload="none" poster='http://content.bitsontherun.com/thumbs/3XnJSIm4-480.jpg'
+        controls preload="none" poster='{{ $video->poster }}'
         data-setup='{ "playbackRates": [1, 1.5, 2] }'>
          <source src="https://player.vimeo.com/external/295694148.m3u8?s=18ac64a43a5451767174c13df7e0e54b3f471a0b" type="application/x-mpegURL">
         @if($video->track_file)
