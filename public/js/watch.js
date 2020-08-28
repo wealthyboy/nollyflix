af8aa5bf00b422be492a705e0d2abf456ac043f7
Jacob Atam <jacob.atam@gmail.com>
2020-08-28 23:11:49 +0100

    Added track file

--- public/js/watch.js
+++ public/js/watch.js
@@ -1,3 +1,4 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
 
 videojs('video', {
       html5: {
@@ -13,26 +14,9 @@ videojs('video', {
 
 });
 
-$(function(){
-  var $refreshButton = $('#refresh');
-  var $results = $('#css_result');
-  
-  function refresh(){
-    var css = $('style.cp-pen-styles').text();
-    $results.html(css);
-  }
-
-  refresh();
-  $refreshButton.click(refresh);
-  
-  // Select all the contents when clicked
-  $results.click(function(){
-    $(this).select();
-  });
-});
 
 
-//document.addEventListener('contextmenu', event => event.preventDefault());
+
 
 // var video = document.querySelector('video');
 
