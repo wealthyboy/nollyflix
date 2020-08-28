97d7553557543ea997313333515182ce7b89c12a
Jacob Atam <jacob.atam@gmail.com>
2020-08-28 22:47:35 +0100

    Added track file

--- resources/views/layouts/watch.blade.php
+++ resources/views/layouts/watch.blade.php
@@ -18,16 +18,13 @@
 		<link rel="dns-prefetch" href="//fonts.gstatic.com">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;500;700&family=Lato:wght@300;400;700&display=swap">
 		<link href="https://vjs.zencdn.net/7.8.4/video-js.css" rel="stylesheet" />
-
-		<link href="https://vjs.zencdn.net/7.8.4/video-js.css" rel="stylesheet" />
 		<link rel="stylesheet" href="/css/watch.css">
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 	
 	</head>
 	<body>
-	
-		
+
 		<section>
 			@yield('content')
 		</section>
