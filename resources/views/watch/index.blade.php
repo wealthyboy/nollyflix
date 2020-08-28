002f05cb2bf379fca51fc3571581174927a332e8
Jacob Atam <jacob.atam@gmail.com>
2020-08-28 23:02:11 +0100

    Added track file

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -10,8 +10,8 @@
     display: flex;
   } 
 </style>
-<div>
-    <video id="background_video" class="video-js vjs-default-skin"  data-setup ='{}'
+<div class="background_video">
+    <video id="video" class="video-js vjs-default-skin"  data-setup ='{}'
         controls preload="none" poster='{{ $video->poster }}'
         data-setup='{ "playbackRates": [1, 1.5, 2] }'>
          <source src="https://player.vimeo.com/external/295694148.m3u8?s=18ac64a43a5451767174c13df7e0e54b3f471a0b" type="application/x-mpegURL">
