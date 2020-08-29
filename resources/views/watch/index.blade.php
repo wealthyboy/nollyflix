94f0172f2e6e0a18a43a7ac41eeb2a3739b0d0be
Jacob Atam <jacob.atam@gmail.com>
2020-08-29 06:28:26 +0100

    Added track file

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -14,7 +14,7 @@
     <video id="video" class="video-js vjs-default-skin"  data-setup ='{}'
         controls preload="none" poster='{{ $video->poster }}'
         data-setup='{ "playbackRates": [1, 1.5, 2] }'>
-         <source src="https://player.vimeo.com/external/295694148.m3u8?s=18ac64a43a5451767174c13df7e0e54b3f471a0b">
+         <source src="{{ $video->link }}">
         @if($video->track_file)
         <track src="{{ $video->track_file }}" kind="subtitles" srclang="en" label="English">
         @endif
