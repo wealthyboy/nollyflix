a154707601f51124c8c6a74dce4e63f8d5223aba
Jacob Atam <jacob.atam@gmail.com>
2020-08-29 06:35:17 +0100

    Added track file

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -10,11 +10,12 @@
     display: flex;
   } 
 </style>
+
 <div class="background_video">
     <video id="video" class="video-js vjs-default-skin"  data-setup ='{}'
         controls preload="none" poster='{{ $video->poster }}'
         data-setup='{ "playbackRates": [1, 1.5, 2] }'>
-         <source src="{{ $video->link }}">
+         <source src="{{ $video->link }}" type="application/x-mpegURL">
         @if($video->track_file)
         <track src="{{ $video->track_file }}" kind="subtitles" srclang="en" label="English">
         @endif
