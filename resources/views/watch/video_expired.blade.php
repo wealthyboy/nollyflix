ef616d630670af35f6c64485cc715277e96ef375
Jacob Atam <jacob.atam@gmail.com>
2020-09-21 00:49:16 +0100

    Added users profile

--- resources/views/watch/video_expired.blade.php
+++ resources/views/watch/video_expired.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.profile')
+@section('page-css')
+<link rel="stylesheet" href="/css/trailer.css">
+@stop
 
 @section('content')
 @include('partials.video_show')
 @endsection
+
+@section('page-scripts')
+<script src="/js/trailer.js?version={{ str_random(6) }}"></script><!---->
+@stop
