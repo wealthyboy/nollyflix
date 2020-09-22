b9219c43e709247e12f595bc14ac4cb597e7b70f
Jacob Atam <jacob.atam@gmail.com>
2020-09-22 20:01:04 +0100

    Updated

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -8,7 +8,7 @@
     <video id="video"  poster="{{ $video->poster }}" class="video-js vjs-default-skin "  muted data-setup ='{}'
         controls 
         data-setup='{ "playbackRates": [1, 1.5, 2] }'>
-        <source src="{{ $video->link }}" type="video/mp4">
+        <source src="{{ $video->link }}" type="application/x-mpegURL">
         @if(optional($video)->track_file)
         <track src="{{ optional($video)->track_file }}" kind="subtitles" srclang="en" label="English">
         @endif
