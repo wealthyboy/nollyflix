1d5b0e464a44b84114f6de13b15abc477864f628
Jacob Atam <jacob.atam@gmail.com>
2020-09-22 22:05:19 +0100

    Updated

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -2,7 +2,6 @@
 @extends('layouts.watch')
 
 @section('content')
-
 <div id="video-page-title-pro" style="background-image:url({{ $video->poster }});">
     <img class="image" src="https://assets.nflxext.com/en_us/pages/wiplayer/site-spinner.png" alt="" width="120" height="120">
 </div><!-- close #video-page-title-pro -->
@@ -12,8 +11,8 @@
     <video id="video"  poster="{{ $video->poster }}" class="video-js vjs-default-skin "  muted data-setup ='{}'
         controls 
         data-setup='{ "playbackRates": [1, 1.5, 2] }'>
-        <source src="/videos/shayfeen.mp4" type="video/mp4">
-        @if(optional($video)->track_file)
+        <source src="{{ $video->link }}" type="application/x-mpegURL">       
+         @if(optional($video)->track_file)
         <track src="{{ optional($video)->track_file }}" kind="subtitles" srclang="en" label="English">
         @endif
     </video>
