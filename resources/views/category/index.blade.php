420fcd48f8f8db2f5734504362e2d5bc92cdc887
Jacob Atam <jacob.atam@gmail.com>
2020-09-23 13:08:57 +0100

    Updated video style

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -6,43 +6,41 @@
 
 <section class="section-content">
 <div id="content-pro">
-    
     <div class="container-fluid custom-gutters-pro">
-        
         <div style="height:15px;"></div>
-        @if($category->sections->count())
-            @foreach($category->sections as $section)
-            <h2 class="post-list-heading">{{ $section->name }}<span></span></h2>
-        
-            <div class="progression-studios-elementor-carousel-container progression-studios-always-arrows-on">
-                <div id="progression-video-carousel" class="owl-carousel progression-carousel-theme">
+         @if($category->sections->count())
+         @foreach($category->sections as $section)
+            <h2 class="post-list-heading ">{{ $section->name }}<span></span></h2>
+            
+            <div class="progression-studios-elementor-carousel-container mb-5 progression-studios-always-arrows-on">
+                <div id="progression-video-carousel" class="owl-carousel progression-video-carousel progression-carousel-theme">
                     @foreach($section->videos as $video)
+                        <div class="item">
+                            <div class="progression-studios-video-index-container">
+                                <a href="/browse/{{ $video->slug }}">
+                                    <div class="progression-studios-video-feaured-image"><img src="{{ $video->tn_poster }}" alt="{{ $video->title }}"></div>
+                                    <div class="progression-video-index-content">
+                                        <div class="progression-video-index-table">
+                                            <div class="progression-video-index-vertical-align">
+                                                <h2 class="progression-video-title"></h2>
+                                                <div class="clearfix"></div> 
+											
+													                                   
+                                            </div><!-- close .progression-video-index-vertical-align -->
+                                        </div><!-- close .progression-video-index-table -->
+                                    </div><!-- close .progression-video-index-content -->
+                                    <div class="video-index-border-hover"></div>
+                                </a>
+                            </div><!-- close .progression-studios-video-index-container  -->
+                            <div class="d-flex position-absolute links-section flex-column  justify-content-center ">
+                                <div class="mx-auto buy-rent-links">
+                                    <a href="{{ optional($video)->preview_link }}" class="btn anchor-btn rounded-0"  data-fancybox id=""><i class="far fa-play-circle"></i>Play Trailer </a>
+                                    <a href="/browse/{{ $video->slug }}" class="btn anchor-btn rounded-0"  id=""><i class="fas fa-shopping-cart"></i>Buy {{ $video->currency }}{{ number_format($video->converted_buy_price) }} </a>
+                                    <a href="/browse/{{ $video->slug }}" class="btn anchor-btn rounded-0"  id=""><i class="fas fa-shopping-cart"></i>Rent {{ $video->currency }}{{ number_format($video->converted_rent_price) }} </a>
 
-                    <div class="item">
-                        <div class="progression-studios-video-index-container">
-                            <a href="/browse/{{ $video->slug }}">
-                                <div class="progression-studios-video-feaured-image"><img src="{{ $video->tn_poster }}" alt="{{ $video->title }}"></div>
-                                <div class="progression-video-index-content">
-                                    <div class="progression-video-index-table">
-                                        <div class="progression-video-index-vertical-align">
-                                            <h2 class="progression-video-title"></h2>
-                                            <div class="clearfix"></div> 
-                                        
-                                                                               
-                                        </div><!-- close .progression-video-index-vertical-align -->
-                                    </div><!-- close .progression-video-index-table -->
-                                </div><!-- close .progression-video-index-content -->
-                                <div class="video-index-border-hover"></div>
-                            </a>
-                        </div><!-- close .progression-studios-video-index-container  -->
-                        <div class="d-flex position-absolute links-section flex-column  justify-content-center ">
-                            <div class="mx-auto buy-rent-links">
-                                <a href="{{ optional($video)->preview_link }}" class="btn anchor-btn rounded-0"  data-fancybox id=""><i class="far fa-play-circle"></i>Play Trailer </a>
-                                <a href="/browse/{{ $video->slug }}" class="btn anchor-btn rounded-0"  id=""><i class="fas fa-shopping-cart">Buy {{ $video->currency }}{{ number_format($video->converted_buy_price) }} </a>
-                                <a href="/browse/{{ $video->slug }}" class="btn anchor-btn rounded-0"  id=""><i class="fas fa-shopping-cart">Rent {{ $video->currency }}{{ number_format($video->converted_rent_price) }} </a>
-                            </div>
-                        </div><!-- close #video-post-buttons-container -->
-                    </div><!-- close .item -->
+                                </div>
+                            </div><!-- close #video-post-buttons-container -->
+                        </div><!-- close .item -->
                     
                     @endforeach
 
@@ -54,10 +52,9 @@
         @endif            
     
         <div class="clearfix"></div>
-    </div><!-- close .container -->
-    
-    
-    
+        
+        </div>
+    </div><!-- close .container --> 
 </div><!-- close #content-pro -->
 </section>
 
