71244387ef217730f252ffc72262e50edc7b901a
Jacob Atam <jacob.atam@gmail.com>
2020-09-25 20:31:09 +0100

    Removed orders

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -27,9 +27,7 @@ class OrdersController extends Controller{
 
 	public function index ( ) 
 	{ 
-		\Schema::disableForeignKeyConstraints();
-		Order::truncate();
-		\Schema::enableForeignKeyConstraints();
+		
 		$orders = Order::orderBy('created_at','desc')->get();
         return view('admin.orders.index',compact('orders'));
     }
