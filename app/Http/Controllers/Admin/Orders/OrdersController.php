5ddf3ba77fd5f7721e9444182c72548a0d319ed3
Jacob Atam <jacob.atam@gmail.com>
2020-09-25 20:25:30 +0100

    Removed orders

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -27,7 +27,7 @@ class OrdersController extends Controller{
 
 	public function index ( ) 
 	{ 
-		Order::truncate();
+		OrderVideo::truncate();
 		$orders = Order::orderBy('created_at','desc')->get();
         return view('admin.orders.index',compact('orders'));
     }
