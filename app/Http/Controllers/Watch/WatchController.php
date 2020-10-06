ae16b554ea0ba2f047331cdb8cedf1d3ad776501
Jacob Atam <jacob.atam@gmail.com>
2020-10-07 00:26:08 +0100

    Updated Rent

--- app/Http/Controllers/Watch/WatchController.php
+++ app/Http/Controllers/Watch/WatchController.php
@@ -43,7 +43,7 @@ class WatchController extends Controller
      */
     public function expired($id)
     {
-        $video = Video::find($id);
+        $video = Video::find($id)->video;
 
         return view('watch.video_expired',compact('video'));
     }
