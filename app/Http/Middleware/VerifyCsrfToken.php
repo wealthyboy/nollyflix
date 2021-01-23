d6c4eef467ce6bf185c7246e4a20af69fcfdc201
Atam Jacob <jacob.atam@gmail.com>
2021-01-23 13:28:19 -0800

    Updated Nollyflix

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'payment/successful'
+        'payment/successful',
+        'webhook/*',
     ];
 }
