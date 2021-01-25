8d0f00245e3ee50eb56f118f0c5fe47a6c58cabc
Atam Jacob <jacob.atam@gmail.com>
2021-01-25 04:28:53 -0800

    Updated REgister

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -2,15 +2,15 @@
 @extends('layouts.watch')
 
 @section('content')
-<div id="video-page-title-pro" style="background-image:url({{ $video->poster }});">
+<div id="video-page-title-pro" style="background-image:url({{ optional($video)->poster }});">
     <img class="spinner-image" src="https://assets.nflxext.com/en_us/pages/wiplayer/site-spinner.png" alt="" width="120" height="120">
 </div><!-- close #video-page-title-pro -->
 <div  class="background_video">
     <div class="back"><a href="/"> <i class="fas fa-long-arrow-alt-left"></i></a></div>
-    <video id="video"  poster="{{ $video->poster }}"  muted  disablePictureInPicture nodownload  class="video-js vjs-default-skin"   
+    <video id="video"  poster="{{ optional($video)->poster }}"  muted  disablePictureInPicture nodownload  class="video-js vjs-default-skin"   
         controls 
         >
-        <source src="{{ $video->link }}" type="application/x-mpegURL">       
+        <source src="{{ optional($video)->link }}" type="application/x-mpegURL">       
          @if(optional($video)->track_file)
         <track src="{{ optional($video)->track_file }}" kind="subtitles" srclang="en" label="English">
         @endif
