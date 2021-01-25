561c68ba98efaca95bc7a298799ee34c6e92ff10
Atam Jacob <jacob.atam@gmail.com>
2021-01-25 04:08:45 -0800

    Updated REgister

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -113,11 +113,9 @@ class RegisterController extends Controller
 			]);
 		} 
 		
-		dd($data);        			
-
 
 		return Validator::make($data, [
-			'first_name' => ['required', 'string', 'max:255'],
+			'name' => ['required', 'string', 'max:255'],
 			'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
