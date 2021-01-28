c1a822fd1c01700f5ff496018b755ebd7be607e0
Atam Jacob <jacob.atam@gmail.com>
2021-01-28 06:45:37 -0800

    Updated  film makers

--- app/Http/Controllers/Admin/Filmers/FilmersController.php
+++ app/Http/Controllers/Admin/Filmers/FilmersController.php
@@ -28,8 +28,6 @@ class FilmersController extends Controller
      */
     public function index()
     {   
-        $user = User::withTrashed()->where(["email"=>'sneezefilms@yahoo.com'])->forcedelete();
-        dd($user);
         $filmers = (new User())->filmers()->latest()->get();
         return   view('admin.filmers.index', compact('filmers'));  
     }
