39ab33ca819bd706fb783889610d934ac6e6d790
Atam Jacob <jacob.atam@gmail.com>
2021-03-21 03:00:31 -0700

    Removed bug

--- app/Video.php
+++ app/Video.php
@@ -239,7 +239,7 @@ class Video extends Model
     }
 
 
-    public function getYearReleaseAttribute(){
-        return null !== $this->release_date?  $this->release_date->format('Y') : null
+    public function getYearReleaseAttribute() {
+        return null !== $this->release_date ?  $this->release_date->format('Y') : null;
     }
 }
