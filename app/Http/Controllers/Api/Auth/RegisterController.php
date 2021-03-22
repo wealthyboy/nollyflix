f14a589eeacebf7e58c1bcf8157eb977e977d27a
Atam Jacob <jacob.atam@gmail.com>
2021-03-22 12:31:00 -0700

    Updated auh

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -12,7 +12,7 @@ class RegisterController extends Controller
 {
     public function action(RegisterRequest $request)
     {
-        $user = User::create([
+        $user   = User::create([
                         'name' => $request->first_name,
                         'last_name' => $request->last_name,
                         'email'=> $request->email,
