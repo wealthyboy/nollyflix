6cb872963e7e4b6be60a939fe5476b48add25478
Atam Jacob <jacob.atam@gmail.com>
2021-03-23 03:09:33 -0700

    Removed Models

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\PrivateUserResource;
-use App\Models\User;
+use App\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -13,10 +13,10 @@ class RegisterController extends Controller
     public function action(RegisterRequest $request)
     {
         $user   = User::create([
-                        'name' => $request->first_name,
-                        'last_name' => $request->last_name,
-                        'email'=> $request->email,
-                        'password' => bcrypt($request->password)
+                        'name'      =>  $request->first_name,
+                        'last_name' =>  $request->last_name,
+                        'email'     =>  $request->email,
+                        'password'  =>  bcrypt($request->password)
                     ]);
 
         return new PrivateUserResource($user);
