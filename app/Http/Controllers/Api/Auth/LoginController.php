f0500606199a54dea42b4844c9c2c532c6ba0cbf
Atam Jacob <jacob.atam@gmail.com>
2021-03-28 02:00:28 -0700

    Udated token

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -6,14 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\PrivateUserResource;
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\Facades\JWTAuth; 
-
 
 class LoginController extends Controller
 {
     public function action(LoginRequest $request)
     {
-        if (!$token = JWTAuth::attempt($request->only('email', 'password'))) {
+        if (!$token = \JWTAuth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'errors' => [
                     'email' => ['Could not sign you in with those details.']
@@ -21,14 +19,10 @@ class LoginController extends Controller
             ], 422);
         }
 
-        $payload = JWTAuth::setToken($token)->getPayload();
-
-
-
         return (new PrivateUserResource($request->user()))
             ->additional([
                 'meta' => [
-                    'token' => $payload
+                    'token' => $token
                 ]
             ]);
     }
