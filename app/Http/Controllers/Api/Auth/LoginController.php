f9403d4fdad527e31f7e398cad7f339eee437e66
Atam Jacob <jacob.atam@gmail.com>
2021-03-27 23:55:34 -0700

    Udated token

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
             ], 422);
         }
 
-        $payload = JWTAuth::getPayload($token);
+        $payload = \JWTAuth::getPayload($token);
 
         
 
