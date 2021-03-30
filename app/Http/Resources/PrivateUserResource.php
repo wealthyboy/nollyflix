531913cff127ceff88d86c498d2ee0043b4ed5e2
Atam Jacob <jacob.atam@gmail.com>
2021-03-30 07:09:12 -0700

    Updated params

--- app/Http/Resources/PrivateUserResource.php
+++ app/Http/Resources/PrivateUserResource.php
@@ -18,7 +18,7 @@ class PrivateUserResource extends JsonResource
             'id' => $this->id,
             'email' => $this->email,
             'name' => $this->name,
-            'videos' => $this->profile_videos
+            'videos' => $this->movies->load('video')
         ];
     }
 }
