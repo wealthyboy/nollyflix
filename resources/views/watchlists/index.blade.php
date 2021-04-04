ba78b488a57229abbe6e7814190beb0081a0701f
Atam Jacob <jacob.atam@gmail.com>
2021-04-04 10:44:54 -0700

    Added cart user relationship

--- resources/views/watchlists/index.blade.php
+++ resources/views/watchlists/index.blade.php
@@ -43,14 +43,10 @@
                                             <div class="progress-bar bg-danger" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                         <div class="ml-2">
-                                            <div class="progression-video-title">{{ optional($video->video)->title }}</div>
-
-                                            <div class="progression-video-title">Type: {{ $video->purchase_type === 'Rent' ? 'Rented' : 'Purchased' }}</div>
-                                            <div class="progression-video-title">Price:  {{ optional(optional($video->orders)->first())->currency }}{{ number_format($video->customer_price) }}</div>
                                             @if ($video->purchase_type == 'Rent')
                                                <div class="progression-video-title">Expires:  {{  $video->isVideoRentExpired() ? 'Video Expired' : $video->videoExpires()->format('d/m/y')  }}</div>
+                                               <div class="progression-video-title">Date:  {{  $video->created_at->format('d/m/y')  }}</div>
                                             @endif
-                                            <div class="progression-video-title">Date:  {{  $video->created_at->format('d/m/y')  }}</div>
                                         </div>
                                        
                                 
