16ee250cf7ecd1d31d52733753e33b82e8c397c3
Atam Jacob <jacob.atam@gmail.com>
2021-04-04 13:02:32 -0700

    Compiled video js

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -5,7 +5,7 @@
 <div id="video-page-title-pro" style="background-image:url({{ optional($video)->poster }});">
     <img class="spinner-image" src="/images/loaders/spinner.png" alt="" width="120" height="120">
 </div><!-- close #video-page-title-pro -->
-<div  class="background_video ">
+<div  class="background_video hide">
     <div class="back">
         <a href="/"> <i class="fas fa-long-arrow-alt-left"></i></a>
     </div>
