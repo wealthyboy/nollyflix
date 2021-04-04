131091f8c51ce1a557ff8881c85ed548b0520f39
Atam Jacob <jacob.atam@gmail.com>
2021-04-04 13:41:19 -0700

    Compiled video js

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -3,13 +3,13 @@
 
 @section('content')
 <div id="video-page-title-pro" style="background-image:url({{ optional($video)->poster }});">
-    <img class="spinner-image" src="/images/loaders/spinner.png" alt="" width="120" height="120">
+    <img class="spinner-image" src="/images/loaders/spinner.png" alt="" width="70" height="70">
 </div><!-- close #video-page-title-pro -->
 <div  class="background_video hide">
     <div class="back">
         <a href="/"> <i class="fas fa-long-arrow-alt-left"></i></a>
     </div>
-    <video id="video"    muted  disablePictureInPicture nodownload  class="video-js vjs-default-skin"   
+    <video id="video"  poster="{{ optional($video)->poster }}"  muted  disablePictureInPicture nodownload  class="video-js vjs-default-skin"   
         controls 
         >
         <source src="{{ optional($video)->link }}" type="application/x-mpegURL">       
