541ee44ba3eefbbe780533dd7c70881d35b0531e
Atam Jacob <jacob.atam@gmail.com>
2021-04-04 11:57:27 -0700

    Compiled video js

--- resources/views/layouts/watch.blade.php
+++ resources/views/layouts/watch.blade.php
@@ -20,7 +20,6 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;500;700&family=Lato:wght@300;400;700&display=swap">
 
 		<link href="/css/video.css?version={{ str_random(6) }}" rel="stylesheet" />
-		<link rel="stylesheet" href="/css/watch.css?version={{ str_random(6) }}">
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 	</head>
