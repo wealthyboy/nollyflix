219f9b37507fbec9a8cc63a52b9b2b0d6d2fef1f
Atam Jacob <jacob.atam@gmail.com>
2021-04-04 13:15:44 -0700

    Compiled video js

--- resources/js/watch.js
+++ resources/js/watch.js
@@ -1,37 +1,37 @@
 import videojs from 'video.js'
 
 
-var  bV = document.querySelector('.background_video')
-var  bK = document.querySelector('.back')
+// var  bV = document.querySelector('.background_video')
+// var  bK = document.querySelector('.back')
 
-bV.addEventListener('mouseenter',function(e) {
-   bK.style.display ="block"
-   bK.style.opacity ="1"
-})
+// bV.addEventListener('mouseenter',function(e) {
+//    bK.style.display ="block"
+//    bK.style.opacity ="1"
+// })
 
-bV.addEventListener('mouseleave',function(e) {
-  bK.style.display ="none"
-  bK.style.opacity ="0"
-})
+// bV.addEventListener('mouseleave',function(e) {
+//   bK.style.display ="none"
+//   bK.style.opacity ="0"
+// })
 
-const vidjs = videojs('video', {
-  html5: {
-    hls: {
-      overrideNative: !videojs.browser.IS_SAFAR}
-  },
-  autoplay:true
-})
+// const vidjs = videojs('video', {
+//   html5: {
+//     hls: {
+//       overrideNative: !videojs.browser.IS_SAFAR}
+//   },
+//   autoplay:true
+// })
 
-console.log(vidjs.getAllPlayers.length)
+// console.log(vidjs.getAllPlayers.length)
 
 
 
-vidjs.ready(function() {
-  document.getElementById("video-page-title-pro").classList.add('hide')
+// vidjs.ready(function() {
+//   document.getElementById("video-page-title-pro").classList.add('hide')
 
-  this.muted(false)
-  console.log("Video loaded")
-});
+//   this.muted(false)
+//   console.log("Video loaded")
+// });
 
 
 
--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -5,18 +5,6 @@
 <div id="video-page-title-pro" style="background-image:url({{ optional($video)->poster }});">
     <img class="spinner-image" src="/images/loaders/spinner.png" alt="" width="120" height="120">
 </div><!-- close #video-page-title-pro -->
-<div  class="background_video">
-    <div class="back">
-        <a href="/"> <i class="fas fa-long-arrow-alt-left"></i></a>
-    </div>
-    <video id="video"  poster="{{ optional($video)->poster }}"  muted  disablePictureInPicture nodownload  class="video-js vjs-default-skin"   
-        controls 
-        >
-        <source src="{{ optional($video)->link }}" type="application/x-mpegURL">       
-         @if($video->track_file)
-        <track src="{{ optional($video)->track_file }}" kind="subtitles" srclang="en" label="English">
-        @endif
-    </video>
-</div><!-- close #content-pro -->
+
 <!-- type="application/x-mpegURL" -->
 @endsection
