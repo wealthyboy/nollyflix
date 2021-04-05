adfa20570477e3a43057082a5ec86f6ba4ca36ad
Atam Jacob <jacob.atam@gmail.com>
2021-04-05 07:47:34 -0700

    Fixed orders

--- database/migrations/2021_04_05_144150_alter_orders_table_add_content_owner_id.php
+++ database/migrations/2021_04_05_144150_alter_orders_table_add_content_owner_id.php
@@ -14,7 +14,7 @@ class AlterOrdersTableAddContentOwnerId extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->integer("content_owner_id")->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AlterOrdersTableAddContentOwnerId extends Migration
     public function down()
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->dropColumn('content_owner_id');       
         });
     }
 }
