b77426a7451a1a50461468b54ea484c53b587308
Atam Jacob <jacob.atam@gmail.com>
2021-04-05 07:36:23 -0700

    Update watch

--- app/Http/Controllers/ProfileWatchList/ProfileWatchListController.php
+++ app/Http/Controllers/ProfileWatchList/ProfileWatchListController.php
@@ -23,14 +23,7 @@ class ProfileWatchListController extends Controller
     public function index()
     {   
         $user = auth()->user();
-        \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-
-        Cart::truncate();
-        Order::truncate();
-
-
-        \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
+        
         $active = "watchlists";
         $page_title = "Buy ,Rent Movies, Watchlists for {$user->fullname()}";
         $page_meta_description = "Buy,Rent nollywood movies";
