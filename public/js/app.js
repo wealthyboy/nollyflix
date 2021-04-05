94dd3640c738f72f8a49cf492b7d209354cfe0c3
Atam Jacob <jacob.atam@gmail.com>
2021-04-05 02:47:01 -0700

    compiled css

--- public/js/app.js
+++ public/js/app.js
@@ -37743,7 +37743,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(179:15) Missed semicolon\n\n \u001b[90m 177 | \u001b[39m\u001b[33m.flex-control-paging\u001b[39m li a.flex-active \u001b[33m{\u001b[39m\n \u001b[90m 178 | \u001b[39m\tcolor\u001b[33m:\u001b[39m\u001b[35m#ffffff\u001b[39m\u001b[33m;\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 179 | \u001b[39m\tborder-color\u001b[33m:\u001b[39m\u001b[35m#E50914\u001b[39m\n \u001b[90m     | \u001b[39m\t             \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 180 | \u001b[39m\tbackground\u001b[33m:\u001b[39m\u001b[35m#E50914\u001b[39m\t\n \u001b[90m 181 | \u001b[39m\u001b[33m}\u001b[39m\n\n    at /Applications/XAMPP/xamppfiles/htdocs/nollyflix/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Applications/XAMPP/xamppfiles/htdocs/nollyflix/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Applications/XAMPP/xamppfiles/htdocs/nollyflix/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Applications/XAMPP/xamppfiles/htdocs/nollyflix/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at /Applications/XAMPP/xamppfiles/htdocs/nollyflix/node_modules/postcss-loader/src/index.js:207:9\n    at processTicksAndRejections (internal/process/task_queues.js:93:5)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
