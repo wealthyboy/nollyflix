a75c187d770122a1500bcecb907364cd308be472
Atam Jacob <jacob.atam@gmail.com>
2021-04-06 03:44:10 -0700

    Add  video rent expires migration

--- app/Http/Controllers/ProfileWatchList/ProfileWatchListController.php
+++ app/Http/Controllers/ProfileWatchList/ProfileWatchListController.php
@@ -23,7 +23,14 @@ class ProfileWatchListController extends Controller
     public function index()
     {   
         $user = auth()->user();
-        
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        Cart::truncate();
+        Order::truncate();
+
+
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         $active = "watchlists";
         $page_title = "Buy ,Rent Movies, Watchlists for {$user->fullname()}";
         $page_meta_description = "Buy,Rent nollywood movies";
