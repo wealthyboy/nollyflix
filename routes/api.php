0250616ccf9d9f3c05c94c6fbf2d4fed9864ca11
Atam Jacob <jacob.atam@gmail.com>
2021-04-13 11:15:09 -0700

    Added Watchlist

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ Route::get('browse',          'Api\Browse\BrowseController@index');
 Route::get('featured_videos', 'Api\Browse\BrowseController@featuredVideos');
 Route::get('browse/casts',    'Api\Casts\CastsController@index');
 Route::get('browse/filmers',  'Api\FilmMakers\FilmMakersController@index');
+Route::get('profile/videos',  'Api\FilmMakers\FilmMakersController@index');
+
 
 
 
@@ -16,6 +18,5 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('register', 'Api\Auth\RegisterController@action');
     Route::post('login', 'Api\Auth\LoginController@action');
     Route::get('me', 'Api\Auth\MeController@action');
-    Route::get('videos',  'Api\FilmMakers\FilmMakersController@index');
 });
 
