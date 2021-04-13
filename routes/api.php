6f4d85cce8406af19f2bbc8d0464353cd3a982f0
Atam Jacob <jacob.atam@gmail.com>
2021-04-13 11:19:03 -0700

    Added Watchlist

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::get('browse',          'Api\Browse\BrowseController@index');
 Route::get('featured_videos', 'Api\Browse\BrowseController@featuredVideos');
 Route::get('browse/casts',    'Api\Casts\CastsController@index');
 Route::get('browse/filmers',  'Api\FilmMakers\FilmMakersController@index');
-Route::get('profile/videos',  'Api\FilmMakers\FilmMakersController@index');
+Route::get('profile/videos',  'Api\ProfileWatchList\WatchListController@index');
 
 
 
