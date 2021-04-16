2845f754cf6b7fc4e472d637431f9e9d49971149
Atam Jacob <jacob.atam@gmail.com>
2021-04-15 20:27:34 -0700

    Upaded search html

--- app/Http/Controllers/Search/SearchController.php
+++ app/Http/Controllers/Search/SearchController.php
@@ -32,17 +32,16 @@ class SearchController extends Controller
             $filtered_array = $request->only(['q', 'field']);
 
 			$filtered_array = array_filter($filtered_array);
-			$query = Video::whereHas('sections', function( $query ) use ( $filtered_array ){
-                $query->where('sections.name','like','%' .$filtered_array['q'] . '%')
+			$query = Video::whereHas('filmers', function( $query ) use ( $filtered_array ){
+                $query->where('filmers.name', 'like', '%' .$filtered_array['q'] . '%')
                     ->orWhere('video.title', 'like', '%' .$filtered_array['q'] . '%');
             })->orWhereHas('genres', function( $query ) use ( $filtered_array ){
                 $query->where('genres.name', 'like', '%' .$filtered_array['q'] . '%');
             })->orWhereHas('casts', function( $query ) use ( $filtered_array ){
                 $query->where('casts.name', 'like', '%' .$filtered_array['q'] . '%')
                 ->orWhere('casts.last_name', 'like', '%' .$filtered_array['q'] . '%');
-            })->orWhereHas('filmers', function( $query ) use ( $filtered_array ){
-                $query->where('filmers.name', 'like', '%' .$filtered_array['q'] . '%')
-                ->orWhere('filmers.last_name', 'like', '%' .$filtered_array['q'] . '%');
+            })->orWhereHas('sections', function( $query ) use ( $filtered_array ){
+                $query->where('sections.name', 'like', '%' .$filtered_array['q'] . '%');
             });
 
         }
