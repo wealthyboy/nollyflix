2318608ca9004ca336d2e6ebf168f1c196eaea1c
Atam Jacob <jacob.atam@gmail.com>
2021-05-02 10:10:00 -0700

    Fixed Error

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('browsewwwwwww',          'Api\Browse\BrowseController@indexllllllll');
-// Route::get('featured_videos', 'Api\Browse\BrowseController@featuredVideos');
-// Route::get('browse/casts',    'Api\Casts\CastsController@index');
-// Route::get('browse/filmers',  'Api\FilmMakers\FilmMakersController@index');
-// Route::get('profile/videos',  'Api\WatchList\WatchListController@index');
-// Route::get('search',          'Api\Search\SearchController@search');
-
-
-
-
-
+Route::get('browse',          'Api\Browse\BrowseController@index');
+Route::get('featured_videos', 'Api\Browse\BrowseController@featuredVideos');
+Route::get('browse/casts',    'Api\Casts\CastsController@index');
+Route::get('browse/filmers',  'Api\FilmMakers\FilmMakersController@index');
+Route::get('profile/videos',  'Api\WatchList\WatchListController@index');
+Route::get('search',          'Api\Search\SearchController@search');
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('register', 'Api\Auth\RegisterController@action');
