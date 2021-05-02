ec7c1869f8302dc4c960b063cdae3b5b1618a63c
Atam Jacob <jacob.atam@gmail.com>
2021-05-02 07:54:30 -0700

    Check error

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('browsewwwwwww',          'Api\Browse\BrowseController@indexllllllll');
-Route::get('featured_videos', 'Api\Browse\BrowseController@featuredVideos');
-Route::get('browse/casts',    'Api\Casts\CastsController@index');
-Route::get('browse/filmers',  'Api\FilmMakers\FilmMakersController@index');
-Route::get('profile/videos',  'Api\WatchList\WatchListController@index');
-Route::get('search',          'Api\Search\SearchController@search');
+// Route::get('browsewwwwwww',          'Api\Browse\BrowseController@indexllllllll');
+// Route::get('featured_videos', 'Api\Browse\BrowseController@featuredVideos');
+// Route::get('browse/casts',    'Api\Casts\CastsController@index');
+// Route::get('browse/filmers',  'Api\FilmMakers\FilmMakersController@index');
+// Route::get('profile/videos',  'Api\WatchList\WatchListController@index');
+// Route::get('search',          'Api\Search\SearchController@search');
 
 
 
