b8434b9a6bd99817e7e5418551dd010384865005
Atam Jacob <jacob.atam@gmail.com>
2021-05-11 16:49:11 -0700

    Updated Mobile Payments

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -13,7 +13,7 @@
     </div>
     <video  poster="{{ optional($video)->poster }}"  muted  disablePictureInPicture nodownload  class="video-js vjs-default-skin"   
          
-        >
+    autoplay>
         <source src="{{ optional($video)->preview_link }}" type="video/mp4">       
          @if($video->track_file)
         <track src="{{ optional($video)->track_file }}" kind="subtitles" srclang="en" label="English">
