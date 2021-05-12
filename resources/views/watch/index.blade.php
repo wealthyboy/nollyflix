b8fe5af9f2dc2942c1b1efd057706fcb131c7794
Atam Jacob <jacob.atam@gmail.com>
2021-05-12 04:40:49 -0700

    Updated Mobile Payments

--- resources/views/watch/index.blade.php
+++ resources/views/watch/index.blade.php
@@ -1,17 +1,18 @@
 
 @extends('layouts.watch')
 
-
 @section('content')
-
-<div  class="background_video ">
+<div id="video-page-title-pro" style="background-image:url({{ optional($video)->poster }});">
+    <img class="spinner-image" src="/images/loaders/spinner.png" alt="" width="70" height="70">
+</div><!-- close #video-page-title-pro -->
+<div  class="background_video hide">
     <div class="back">
         <a href="/"> <i class="fas fa-long-arrow-alt-left"></i></a>
     </div>
-    <video  poster="{{ optional($video)->poster }}"  muted  disablePictureInPicture nodownload  class="video-js vjs-default-skin"   
-         
-    autoplay>
-        <source src="{{ optional($video)->preview_link }}" type="video/mp4">       
+    <video id="video"  poster="{{ optional($video)->poster }}"  muted  disablePictureInPicture nodownload  class="video-js vjs-default-skin"   
+        controls 
+        >
+        <source src="{{ optional($video)->link }}" type="application/x-mpegURL">       
          @if($video->track_file)
         <track src="{{ optional($video)->track_file }}" kind="subtitles" srclang="en" label="English">
         @endif
