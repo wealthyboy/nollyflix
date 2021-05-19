13225c3bd3c61f416f0962b1a26cb189c29b8fb0
Atam Jacob <jacob.atam@gmail.com>
2021-05-19 10:08:48 -0700

    made timestamp false

--- app/Http/Controllers/WebHook/WebHookController.php
+++ app/Http/Controllers/WebHook/WebHookController.php
@@ -35,58 +35,29 @@ class WebHookController extends Controller
             
             $input =  $request->data['customer'];
             //The phone_number carries the cart id. The payment process does not allow custom data
-            $cart     =   Cart::find($input['phone_number']);
-            $order = Order::firstOrCreate(
-                ['cart_id' =>  $cart->id],
-                [
-                    'user_id'  => $cart->user->id,
-                    'currency' => '₦',
-                    'invoice'  => "INV-".date('Y')."-".rand(10000,39999),
-                    'video_id' => $cart->video_id,
-                    'video_rent_expires' => now()->addDays(2)
-                ]
-            );
+            $cart_id     =   Cart::find($input['phone_number']);
+         
+		    $order = Order::firstOrCreate(
+			['cart_id' =>  $cart_id],
+			[
+				'user_id'  => $cart->user->id,
+				'currency' => '₦',
+				'invoice'  => "INV-".date('Y')."-".rand(10000,39999),
+				'video_id' => $cart->video_id,
+				'video_rent_expires' => now()->addDays(2)
+			]
+		);
 
+		try {
+			$when = now()->addMinutes(5);
+			\Mail::to($user->email)
+				->bcc($admin_emails[0])
+				->later($when, new OrderReceipt($cart->user, $order, $this->settings,"₦"));
+		} catch (\Throwable $th) {
+			//throw $th;
+		}
 
-            try {
-                $when = now()->addMinutes(5);
-                \Mail::to($user->email)
-                    ->bcc($admin_emails[0])
-                    ->later($when, new OrderReceipt($cart->user, $order, $this->settings,"₦"));
-            } catch (\Throwable $th) {
-                //throw $th;
-            }
-
-            
-
-            return 1;
-
-            if (null ===  $is_order) {
-                $cart     = Cart::find($input['phone_number']);
-                $order->user_id             = $cart->user->id;
-                $order->cart                = $cart->id;
-                $order->currency            =  "NGN";
-                $order->invoice             =  "INV-".date('Y')."-".rand(10000,39999);
-                $order->video_id            = $cart->video_id;
-                $order->video_rent_expires  = now()->addDays(2);
-                $order->save();
-            
-                $admin_emails = explode(',',$this->settings->alert_email);
-                Log::info("Successfull payment");
-
-                try {
-                    $when = now()->addMinutes(5);
-                    \Mail::to($user->email)
-                        ->bcc($admin_emails[0])
-                        ->later($when, new OrderReceipt($user, $order, $this->settings,"NGN"));
-                } catch (\Throwable $th) {
-                    //throw $th;
-                }
-            } 
-            
-        } catch (\Throwable $th) {
-            Log::error($th);
-        }
+		return $order;
     
 
     }
