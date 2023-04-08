aa5fc183372a968695cd67eae07946a7bd0583f8
Jacob <jacob.atam@gmail.com>
2023-04-08 10:54:47 +0100

    U

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
    <div class="col-md-12">
       <div class="text-right">
-        
+
       </div>
    </div>
    <div class="col-md-4">
@@ -17,7 +17,7 @@
                   <tbody>
                      <tr>
                         <td style="width: 1%;"><button data-toggle="tooltip" title="" class="btn btn-info btn-xs" data-original-title="Store"><i class="fa fa-shopping-cart fa-fw"></i></button></td>
-                        <td><a href="" target="_blank">{{  Config('app.name') }}</a></td>
+                        <td><a href="" target="_blank">{{ Config('app.name') }}</a></td>
                      </tr>
                      <tr>
                         <td><button data-toggle="tooltip" title="Date Added" class="btn btn-info btn-xs"><i class="fa fa-calendar fa-fw"></i></button></td>
@@ -27,7 +27,7 @@
                         <td><button data-toggle="tooltip" title="Payment Method" class="btn btn-info btn-xs"><i class="fa fa-credit-card fa-fw"></i></button></td>
                         <td>{{ $order->payment_type }}</td>
                      </tr>
-                    
+
                   </tbody>
                </table>
             </div>
@@ -51,7 +51,7 @@
                         <td><button data-toggle="tooltip" title="E-Mail" class="btn btn-info btn-xs"><i class="fa fa-envelope-o fa-fw"></i></button></td>
                         <td><a href="">{{ $order->user->email }}</a></td>
                      </tr>
-                     
+
                   </tbody>
                </table>
             </div>
@@ -85,95 +85,94 @@
             <i class="material-icons">assignment</i>
          </div>
          <div class="card-content">
-            
-               <h2>Items</h2>
-               <table class="table table-shopping">
-                  <thead>
-                     <tr>
-                        <th >Poster</th>
-                        <th class="th-description">Title</th>
-                        <th class="text-right">Purchase Type</th>
-                        <th class="text-right">Price</th>
-                        <th class="text-right">Qty</th>
-                        <th class="text-right">Amount</th>
-                     </tr>
-                  </thead>
-                  <tbody>
-                     @foreach ( $order->carts as $cart )
-                     <tr>
-                        <td>
-                           <div class="img-container">
-                              <img src="{{ optional($cart->video)->tn_poster  }} " alt="...">
-                           </div>
-                           <div class="form-group label-floating">
-                             <input type="hidden" class="cart_id" value="{{ $cart->id }}" />
-                              <select  class="form-control mt-3 update_status" name="order_status[{{ $cart->id }}]" id="">
-                                 <option value="" >Choose Status</option>
-                                 @foreach($statuses as $status)
-                                   @if ($status == $cart->status)
-                                       <option value="{{ $status }}" selected>{{ $status }}</option>
-                                    @else
-                                      <option value="{{ $status }}">{{ $status }}</option>
-                                    @endif
-                                 @endforeach
-                              </select>
-                           </div>
-                        </td>
-                        <td class="td-name">
-                           <a href="">{{  optional(optional($cart)->video)->title }}</a>
-                           <br><small></small>
-                        </td>
-                        <td>
-                           {{ $cart->purchase_type }}
-                        </td>
-                        <td class="td-number text-right">
-                           {{ $order->currency }}{{ $cart->customer_price }}
-                        </td>
-                        <td class="td-number">
-                           {{ $cart->quantity }}
-                        </td>
-                        <td class="td-number">
-                           <small>{{ $order->currency }}</small>{{ $cart->customer_total }}
-                        </td>
-                        
-                     </tr>
-                     @endforeach                               
-                  </tbody>
-               </table>
-               <table class="table ">
-                  <tfoot>
-                     <tr>
-                        <td colspan="6" class="text-right">Sub-Total</td>
-                        <td class="text-right"><small>{{ $order->currency }}</small>{{ $order->get_total() }}</td>
-                     </tr>
-                   
-                     <tr>
-                        <td colspan="6" class="text-right">Total</td>
-                        <td class="text-right">{{ $order->currency }}{{  $order->get_total() }}</td>
-                     </tr>
-                  </tfoot>
-               </table>
-            </div>
+
+            <h2>Items</h2>
+            <table class="table table-shopping">
+               <thead>
+                  <tr>
+                     <th>Poster</th>
+                     <th class="th-description">Title</th>
+                     <th class="text-right">Purchase Type</th>
+                     <th class="text-right">Price</th>
+                     <th class="text-right">Qty</th>
+                     <th class="text-right">Amount</th>
+                  </tr>
+               </thead>
+               <tbody>
+
+                  <?php $cart = $order->cart  ?>
+                  <tr>
+                     <td>
+                        <div class="img-container">
+                           <img src="{{ optional($cart->video)->tn_poster  }} " alt="...">
+                        </div>
+                        <div class="form-group label-floating">
+                           <input type="hidden" class="cart_id" value="{{ $cart->id }}" />
+                           <select class="form-control mt-3 update_status" name="order_status[{{ $cart->id }}]" id="">
+                              <option value="">Choose Status</option>
+                              @foreach($statuses as $status)
+                              @if ($status == $cart->status)
+                              <option value="{{ $status }}" selected>{{ $status }}</option>
+                              @else
+                              <option value="{{ $status }}">{{ $status }}</option>
+                              @endif
+                              @endforeach
+                           </select>
+                        </div>
+                     </td>
+                     <td class="td-name">
+                        <a href="">{{ optional(optional($cart)->video)->title }}</a>
+                        <br><small></small>
+                     </td>
+                     <td>
+                        {{ $cart->purchase_type }}
+                     </td>
+                     <td class="td-number text-right">
+                        {{ $order->currency }}{{ $cart->customer_price }}
+                     </td>
+                     <td class="td-number">
+                        {{ $cart->quantity }}
+                     </td>
+                     <td class="td-number">
+                        <small>{{ $order->currency }}</small>{{ $cart->customer_total }}
+                     </td>
+
+                  </tr>
+               </tbody>
+            </table>
+            <table class="table ">
+               <tfoot>
+                  <tr>
+                     <td colspan="6" class="text-right">Sub-Total</td>
+                     <td class="text-right"><small>{{ $order->currency }}</small>{{ $order->get_total() }}</td>
+                  </tr>
+
+                  <tr>
+                     <td colspan="6" class="text-right">Total</td>
+                     <td class="text-right">{{ $order->currency }}{{ $order->get_total() }}</td>
+                  </tr>
+               </tfoot>
+            </table>
          </div>
       </div>
    </div>
+</div>
 </div>
 <!-- end row -->
 @endsection
 @section('inline-scripts')
 $(".update_status").on('change',function(e){
-      let self = $(this)
-      if(self.val() == '') return;
-      let value = self.parent().find(".cart_id").val()
-      console.log(value)
-      var payLoad = { cart_id: value,status: self.val() }
-      $.ajax({
-         type: "PATCH",
-         url: "/admin/orders/"+value,
-         data: payLoad,
-      }).done(function(response){
-         console.log(response)
-      })
+let self = $(this)
+if(self.val() == '') return;
+let value = self.parent().find(".cart_id").val()
+console.log(value)
+var payLoad = { cart_id: value,status: self.val() }
+$.ajax({
+type: "PATCH",
+url: "/admin/orders/"+value,
+data: payLoad,
+}).done(function(response){
+console.log(response)
+})
 })
 @stop
-
