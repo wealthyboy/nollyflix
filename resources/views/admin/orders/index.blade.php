a7ac2975d67dd793442d3fc19b51cecc66275523
Jacob <jacob.atam@gmail.com>
2023-04-08 11:13:48 +0100

    U

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -11,22 +11,22 @@
                   <h4 class="card-title">Orders</h4>
                </div>
                <div class="text-right">
-               
+
                </div>
             </div>
             <div class="toolbar">
                <!-- Here you can write extra buttons/actions for the toolbar  -->
             </div>
             <div class="material-datatables">
-            <form action="" method="post" enctype="multipart/form-data" id="form-orders">
-                
-                <table id="datatables" class="table table-striped table-shopping table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+               <form action="" method="post" enctype="multipart/form-data" id="form-orders">
+
+                  <table id="datatables" class="table table-striped table-shopping table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
                      <thead>
                         <tr>
                            <th>
                               <div class="checkbox">
                                  <label>
-                                 <input onclick="$('input[name*=\'selected\']').prop('checked', this.checked);" type="checkbox" name="optionsCheckboxes" >
+                                    <input onclick="$('input[name*=\'selected\']').prop('checked', this.checked);" type="checkbox" name="optionsCheckboxes">
                                  </label>
                               </div>
                            </th>
@@ -38,33 +38,33 @@
                         </tr>
                      </thead>
                      <tbody>
-                       @foreach ($orders as $order )
-                            <tr>
-                            <td>
+                        @foreach ($orders as $order )
+                        <tr>
+                           <td>
                               <div class="checkbox">
                                  <label>
-                                 <input type="checkbox" value="{{ $order->id }}" name="selected[]" >
+                                    <input type="checkbox" value="{{ $order->id }}" name="selected[]">
                                  </label>
                               </div>
                            </td>
-                            <td class="text-left">{{ $order->invoice }}</td>
-                            <td  class="text-left">{{ $order->user->fullname() }}</td>
-                            <td>{{ $order->created_at }}</td>
-                            <td class="text-left">{{ $order->currency  ?? '₦'}}{{ $order->get_total() }}</td>
-                            <td class="td-actions text-center">
-                             
-                              
+                           <td class="text-left">{{ $order->invoice }}</td>
+                           <td class="text-left">{{ $order->user->fullname() }}</td>
+                           <td>{{ $order->created_at }}</td>
+                           <td class="text-left">{{ $order->currency  ?? '₦'}}{{ $order->cart->price }}</td>
+                           <td class="td-actions text-center">
+
+
                               <span><a href="{{ route('admin.orders.show',['order'=>$order->id]) }}" rel="tooltip" class="btn btn-success btn-simple" data-original-title="" title="View">
-                                 <i class="fa fa-eye"></i>
-                              </a></span>
+                                    <i class="fa fa-eye"></i>
+                                 </a></span>
                            </td>
-                        @endforeach   
-                        
+                           @endforeach
+
                      </tbody>
                   </table>
-                  </form>
+               </form>
 
-               
+
             </div>
          </div>
          <!-- end content-->
@@ -79,53 +79,53 @@
 @stop
 @section('inline-scripts')
 function myprint() {
-    window.print();
+window.print();
 }
 $(document).ready(function() {
 
 
 $(".add_to_cart").on('click',function(e){
-    var self = $(this);
-    e.preventDefault();
-    $qty = self.data('quantity');
-    $product_id = self.data('productid');
-    $quantity = self.parent().parent().find('input#quantity').val();
-    $discount = self.parent().parent().find('input#discount').val();
+var self = $(this);
+e.preventDefault();
+$qty = self.data('quantity');
+$product_id = self.data('productid');
+$quantity = self.parent().parent().find('input#quantity').val();
+$discount = self.parent().parent().find('input#discount').val();
 
-    if( $quantity  == ''){
-       alert("Please enter a quantity");
-       return false;
-    }
-    
-    if($quantity >  $qty){
-       alert("Cannot add that amount of quantity");
-       return false;
-    }
+if( $quantity == ''){
+alert("Please enter a quantity");
+return false;
+}
 
-    $("#cart_product_id").val($product_id);
-    $("#cart_discount").val($discount);
-    $("#cart_quantity").val($quantity);
-    $("#add_to_to_cart").attr('action','/add-to-cart/'+$product_id);
-    $("#add_to_to_cart").submit();   
+if($quantity > $qty){
+alert("Cannot add that amount of quantity");
+return false;
+}
+
+$("#cart_product_id").val($product_id);
+$("#cart_discount").val($discount);
+$("#cart_quantity").val($quantity);
+$("#add_to_to_cart").attr('action','/add-to-cart/'+$product_id);
+$("#add_to_to_cart").submit();
 });
 
 
 
 
 $('#datatables').DataTable({
-    "pagingType": "full_numbers",
-    "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-    responsive: true,
-    language: {
-    search: "_INPUT_",
-    searchPlaceholder: "Search records",
-    }
-    });
+"pagingType": "full_numbers",
+"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+responsive: true,
+language: {
+search: "_INPUT_",
+searchPlaceholder: "Search records",
+}
+});
 
-    $('#cat_sub_cat_id').on('change',function(){
-       $name = $(this).find(':selected').data('name');
-       $(this).attr('name', $name);
-    });
-    s.initFormExtendedDatetimepickers();
+$('#cat_sub_cat_id').on('change',function(){
+$name = $(this).find(':selected').data('name');
+$(this).attr('name', $name);
+});
+s.initFormExtendedDatetimepickers();
 });
 @stop
