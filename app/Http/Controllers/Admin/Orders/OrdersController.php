4c1f4453523d8dc1264066be9f308c4ee9295826
Jacob <jacob.atam@gmail.com>
2023-04-08 10:32:15 +0100

    U

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -15,60 +15,60 @@ use App\Cart;
 
 
 
-class OrdersController extends Controller{ 
+class OrdersController extends Controller
+{
 
 
-  
-    public function __construct()
-    {
-       $this->middleware('admin'); 
-	   $this->settings =  \DB::table('system_settings')->first();
-    }
 
-	public function index ( ) 
-	{ 
-		$orders = Order::orderBy('created_at','desc')->get();
-        return view('admin.orders.index',compact('orders'));
-    }
-    
+	public function __construct()
+	{
+		$this->middleware('admin');
+		$this->settings =  \DB::table('system_settings')->first();
+	}
+
+	public function index()
+	{
+		$orders = Order::orderBy('created_at', 'desc')->get();
+		return view('admin.orders.index', compact('orders'));
+	}
+
 
 	public function invoice($id)
 	{
 		$order     =  Order::find($id);
-		$sub_total   =  $order->carts->sum('total');
-        $system_settings = SystemSetting::first();
-        return view('admin.orders.invoice',compact('sub_total','order','system_settings'));
-    }
+		$sub_total   =  $order->cart->sum('total');
+		$system_settings = SystemSetting::first();
+		return view('admin.orders.invoice', compact('sub_total', 'order', 'system_settings'));
+	}
 
-    public static function order_status() { 
+	public static function order_status()
+	{
 		return [
 			"Complete",
 			"Refunded",
 		];
 	}
 
-	public function show($id) 
-	{ 
-       $order       =  Order::find($id);
-	   $sub_total   =  $order->carts->sum('total');
-	   $statuses    =  static::order_status();
-	   return view('admin.orders.show',compact('statuses','order','sub_total'));
+	public function show($id)
+	{
+		$order       =  Order::find($id);
+		$sub_total   =  $order->cart->sum('total');
+		$statuses    =  static::order_status();
+		return view('admin.orders.show', compact('statuses', 'order', 'sub_total'));
 	}
-	
-	public function update(Request $request,$cart_id)
+
+	public function update(Request $request, $cart_id)
 	{
 		$cart         = Cart::findOrFail($cart_id);
 		$cart->status = $request->status;
-		$cart->save();        
+		$cart->save();
 		return $cart;
 	}
-    
-	public function dispatchNote(Request $request,$id)
+
+	public function dispatchNote(Request $request, $id)
 	{
-	    $page_title = 'Dispatch Note';
-		$order      =  Order::find($id);	 
-		return view('admin.dispatch.index',compact('order','page_title'));
+		$page_title = 'Dispatch Note';
+		$order      =  Order::find($id);
+		return view('admin.dispatch.index', compact('order', 'page_title'));
 	}
-
-
 }
--- app/Http/Controllers/WebHook/WebHookController.php
+++ app/Http/Controllers/WebHook/WebHookController.php
@@ -18,53 +18,50 @@ use App\SystemSetting;
 class WebHookController extends Controller
 {
 
-	public  $settings;
+    public  $settings;
 
-	public function __construct()
-	{
-		$this->settings =  SystemSetting::first();
+    public function __construct()
+    {
+        $this->settings =  SystemSetting::first();
     }
-    
 
-    public function payment(Request $request,Order $order)
-    {   
+
+    public function payment(Request $request, Order $order)
+    {
         // if ( !array_key_exists('x-paystack-signature', $_SERVER) ) {
         //     return;
         // }         
         try {
-            
+
             $input =  $request->data['customer'];
             //The phone_number carries the cart id. The payment process does not allow custom data
-            $cart_id     =   Cart::find($input['phone_number']);
-         
-		    $order = Order::updateOrCreate(
-			    ['cart_id' =>  $cart_id],
+            $cart     =   Cart::find($input['phone_number']);
+
+            $order = Order::updateOrCreate(
+                ['cart_id' =>  $cart->id],
                 [
                     'user_id'  => $cart->user->id,
                     'currency' => '₦',
-                    'invoice'  => "INV-".date('Y')."-".rand(10000,39999),
+                    'invoice'  => "INV-" . date('Y') . "-" . rand(10000, 39999),
                     'video_id' => $cart->video_id,
                     'video_rent_expires' => now()->addDays(2)
                 ]
-		    );
+            );
 
             try {
                 $when = now()->addMinutes(5);
                 \Mail::to($user->email)
                     ->bcc($admin_emails[0])
-                    ->later($when, new OrderReceipt($cart->user, $order, $this->settings,"₦"));
+                    ->later($when, new OrderReceipt($cart->user, $order, $this->settings, "₦"));
             } catch (\Throwable $th) {
                 //throw $th;
             }
-
         } catch (\Throwable $th) {
-			//throw $th;
-		}
+            //throw $th;
+        }
 
 
-		return $order;
-    
-
+        return $order;
     }
 
     public function gitHub()
@@ -72,6 +69,4 @@ class WebHookController extends Controller
         $output =  shell_exec('sh /home/forge/nollyflix.tv/deploy.sh');
         return  $output;
     }
-
-   
 }
