d7fd4f82b68ec4ffd898239dec7b0175752564f1
Jacob <jacob.atam@gmail.com>
2026-03-17 10:20:12 +0100

    fix

--- app/Http/Controllers/Api/WatchList/WatchListController.php
+++ app/Http/Controllers/Api/WatchList/WatchListController.php
@@ -24,7 +24,7 @@ class WatchListController extends Controller
     {
         $user = auth()->user();
         return WatchList::collection(
-            $user->movies->load('cart')
+            $user->movies->load('cart', 'vidoe')
         );
     }
 }
