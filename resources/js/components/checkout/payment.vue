d679f7e38a34cc4ccfb915e4255bd6564a38f050
Jacob Atam <jacob.atam@gmail.com>
2026-09-15 13:34:59 +0100

    fix

--- resources/js/components/checkout/payment.vue
+++ resources/js/components/checkout/payment.vue
@@ -162,14 +162,11 @@ export default {
                 this.scriptLoaded.then(() => {
                     var x = FlutterwaveCheckout({
                         public_key:
-                            "FLWPUBK-3c3bd76ddea8a8bc289651bfd883b970-X", //test pbkey FLWPUBK_TEST-d8c9813bd0912d597cc6fddacc11e45f-X,//live  FLWPUBK-3c3bd76ddea8a8bc289651bfd883b970-X
+                            "FLWPUBK-fb7922761d315590b104f8c5f6480c72-X", //test pbkey FLWPUBK_TEST-d8c9813bd0912d597cc6fddacc11e45f-X,//live  FLWPUBK-3c3bd76ddea8a8bc289651bfd883b970-X
                         customer_email: context.user.email,
                         amount: context.price,
                         currency: context.user.iso_code,
-                        payment_options:
-                            context.user.iso_code === "NGN"
-                                ? "card,banktransfer,ussd"
-                                : "card",
+                        country: "NG",
                         tx_ref:
                             "rave-" +
                             Math.floor(Math.random() * 1000000000 + 1),
